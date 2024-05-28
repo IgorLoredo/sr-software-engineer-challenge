3cdd25685d6623300c59c9e6108847d9358afab9
itausegdev <itausegurosdev@gmail.com>
2024-05-28 08:36:52 -0300

    fix: Fix scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
-#Create topics
-kafka-topics --create --topic itausegdev-insurance-quote-received --bootstrap-server broker:9092 --partitions 3 --replication-factor 1
-kafka-topics --create --topic itausegdev-insurance-policy-emitted --bootstrap-server broker:9092 --partitions 3 --replication-factor 1
+# Create topics
+echo "###################################### Creating topics ######################################"
+kafka-topics --bootstrap-server broker:29092 --create --if-not-exists --topic itausegdev-insurance-quote-received --replication-factor 1 --partitions 3 
+kafka-topics --bootstrap-server broker:29092 --create --if-not-exists --topic itausegdev-insurance-policy-emitted --replication-factor 1 --partitions 3 
+echo "###################################### Topics were created successfully ######################################"
 
-curl -X POST -H "Content-Type:application/vnd.schemaregistry.v1+json" --data '{"schema": "'"$(cat /schemas/br.itausegdev.quotes.schemas.insurance_quote_received.avsc)"'"}' http://schema-registry:8081/subjects/itausegdev-insurance-quote-received/versions 
-curl -X POST -H "Content-Type:application/vnd.schemaregistry.v1+json" --data '{"schema": "'"$(cat /schemas/br.itausegdev.policies.schemas.insurance_policy_emitted.avsc)"'"}' http://schema-registry:8081/subjects/itausegdev-insurance-policy-emitted/versions
+echo "###################################### Registering schemas ######################################"
 
+INSURANCE_QUOTE_SCHEMA=$(cat /schemas/br.itausegdev.quotes.schemas.insurance_quote_received.avsc | sed "s/'/\\'/g")
+curl -X POST -H "Content-Type:application/vnd.schemaregistry.v1+json" --data "{\"schema\": \"${INSURANCE_QUOTE_SCHEMA}\"}" http://schema-registry:8081/subjects/itausegdev-insurance-quote-received-value/versions 
+
+INSURANCE_POLICY_SCHEMA=$(cat /schemas/br.itausegdev.policies.schemas.insurance_policy_emitted.avsc | sed "s/'/\\'/g")
+curl -X POST -H "Content-Type:application/vnd.schemaregistry.v1+json" --data "{\"schema\": \"${INSURANCE_POLICY_SCHEMA}\"}" http://schema-registry:8081/subjects/itausegdev-insurance-policy-emitted-value/versions
+
+echo "###################################### Schemas were registered successfully ######################################"
+
+tail -f /dev/null
