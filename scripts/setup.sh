6604c40d6b6b26b64eec1e486ca8a97f232e3427
itausegdev <147622071+itausegdev@users.noreply.github.com>
2024-06-02 20:52:20 -0300

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,12 +8,32 @@ echo "###################################### Topics were created successfully ##
 
 echo "###################################### Registering schemas ######################################"
 
-INSURANCE_QUOTE_SCHEMA=$(cat /schemas/br.itausegdev.quotes.schemas.insurance_quote_received.avsc | sed "s/'/\\'/g")
-curl -X POST -H "Content-Type:application/vnd.schemaregistry.v1+json" --data "{\"schema\": \"${INSURANCE_QUOTE_SCHEMA}\"}" http://schema-registry:8081/subjects/itausegdev-insurance-quote-received-value/versions 
+SCHEMA_REGISTRY_URL=http://schema-registry:8081
 
-INSURANCE_POLICY_SCHEMA=$(cat /schemas/br.itausegdev.policies.schemas.insurance_policy_emitted.avsc | sed "s/'/\\'/g")
-curl -X POST -H "Content-Type:application/vnd.schemaregistry.v1+json" --data "{\"schema\": \"${INSURANCE_POLICY_SCHEMA}\"}" http://schema-registry:8081/subjects/itausegdev-insurance-policy-emitted-value/versions
+FILE_PATH_ONE="/schemas/br.itausegdev.quotes.schemas.insurance_quote_received.avsc"
+SCHEMA_ONE='{"schema":"'$(cat ${FILE_PATH_ONE} | tr '\n' '\r' | sed  's/"/\\"/g' | sed 's/\s//g')'"}'
+SUBJECT_ONE="itausegdev-insurance-quote-received-value"
+URL_ONE="${SCHEMA_REGISTRY_URL}/subjects/${SUBJECT_ONE}/versions" 
 
-echo "###################################### Schemas were registered successfully ######################################"
+cat ${FILE_PATH_ONE} | tr '\n' '\r' | sed  's/"/\\"/g' | sed 's/\s//g'
+
+echo -e "###################################### Registering schema ${FILE_PATH_ONE} ######################################"
+curl -X POST -H 'Content-Type:application/vnd.schemaregistry.v1+json' --data ${SCHEMA_ONE} ${URL_ONE}
+echo -e "###################################### Schema ${FILE_PATH_ONE} was registered successfully ######################################"
+
+
+FILE_PATH_TWO="/schemas/br.itausegdev.policies.schemas.insurance_policy_emitted.avsc"
+SCHEMA_TWO='{"schema":"'$(cat ${FILE_PATH_TWO} | tr '\n' '\r' | sed  's/"/\\"/g' | sed 's/\s//g')'"}'
+SUBJECT_TWO="itausegdev-insurance-policy-emitted-value"
+URL_TWO="${SCHEMA_REGISTRY_URL}/subjects/${SUBJECT_TWO}/versions" 
+
+cat ${FILE_PATH_TWO} | tr '\n' '\r' | sed  's/"/\\"/g' | sed 's/\s//g'
+
+echo -e "###################################### Registering schema ${FILE_PATH_TWO} ######################################"
+curl -X POST -H 'Content-Type:application/vnd.schemaregistry.v1+json' --data ${SCHEMA_TWO} ${URL_TWO}
+echo -e "###################################### Schema ${FILE_PATH_TWO} was registered successfully ######################################"
+
+
+echo -e "###################################### Schemas were registered successfully ######################################"
 
 tail -f /dev/null
